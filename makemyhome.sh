c58f017582b700411ccdfc483ca11d07e0915f65
Kevin O'Connor <96597458+k-oconnor@users.noreply.github.com>
2022-01-31 19:14:30 -0600

    This script 'makes my home'

---
+++ makemyhome.sh
@@ -0,0 +1,20 @@
+mkdir home
+cd ~/Desktop/Repos/eco395m-homework-1/home
+mkdir bathroom
+mkdir livingroom
+mkdir kitchen
+mkdir bedroom
+cd ~/Desktop/Repos/eco395m-homework-1/home/bathroom
+echo "Toilet" > bathroom.txt
+cd ..
+cd ~/Desktop/Repos/eco395m-homework-1/home/livingroom
+echo "Couch" > livingroom.txt
+echo "Television" >> livingroom.txt
+cd ..
+cd ~/Desktop/Repos/eco395m-homework-1/home/kitchen
+echo "Stove" > kitchen.txt
+echo "Pantry" >> kitchen.txt
+cd ..
+cd ~/Desktop/Repos/eco395m-homework-1/home/bedroom
+echo "Bed" > bedroom.txt
+echo "Dresser" >> bedroom.txt
